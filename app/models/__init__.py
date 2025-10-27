2c6cdb11b18aa9324263e066e6e4236beb9836d0
Avinash <kanhamoolchand@gmail.com>
2025-10-27 13:16:03 +0530

    Disable MongoDB model imports

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,17 +1,10 @@
-from .user import User
-from .project import Project
-from .document import Document
-from .comment import Comment
-from .tamper_proof_log import TamperProofLog
-from .ai_validation_result import AIValidationResult
-from .notification import Notification
+# Disabled MongoDB model imports for in-memory mode
+# from .user import User
+# from .project import Project
+# from .document import Document
+# from .comment import Comment
+# from .tamper_proof_log import TamperProofLog
+# from .ai_validation_result import AIValidationResult
+# from .notification import Notification
 
-__all__ = [
-    "User",
-    "Project", 
-    "Document",
-    "Comment",
-    "TamperProofLog",
-    "AIValidationResult",
-    "Notification"
-]
+__all__ = []
